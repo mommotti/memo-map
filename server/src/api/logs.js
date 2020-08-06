afa6ed1163d054f1c022be8a4585a8ff1129cbdc
Matteo Fenu <matfen97@gmail.com>
2020-08-06 23:31:56 +0200

    added a security key for adding a pin to the map

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -14,6 +14,10 @@ router.get('/', async (req, res, next) => {
 })
 router.post('/', async (req, res, next) => {
     try {
+        if (req.get('X-API-KEY') !== process.env.API_KEY) {
+            res.status(401);
+            throw new Error('UnAuthorized');
+        }
         const logEntry = new LogEntry(req.body)
         const createdEntry = await logEntry.save()
         res.json(createdEntry)
