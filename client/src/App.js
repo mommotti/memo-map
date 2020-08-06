aa0c9b70a751e8efc6d3b788de96af9a93c6ab72
Matteo Fenu <matfen97@gmail.com>
2020-08-06 09:27:55 +0200

    added the useState hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker } from 'react-map-gl';
 import { listLogEntries } from './API'
 const App = () => {
+  const [logEntries, setLogEntries] = useState([])
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -9,9 +10,11 @@ const App = () => {
     longitude: -100.99,
     zoom: 4
   });
-  useEffect(() => {
+
+  useEffect(() => { //loads only once
     (async () => {
       const logEntries = await listLogEntries()
+      setLogEntries(logEntries)
       console.log(logEntries)
     })()
   }, [])
@@ -22,7 +25,21 @@ const App = () => {
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       onViewportChange={setViewport}
       mapStyle="mapbox://styles/mapbox/dark-v10"
-    />
+    >
+      {
+        logEntries.map(entry => (
+          <Marker
+            key={entry._id}
+            latitude={entry.latitude}
+            longitude={entry.longitude}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <div>{entry.title}</div>
+          </Marker>
+        ))
+      }
+    </ReactMapGL>
   );
 }
 
