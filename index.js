a8ad830d9cafd56174748b42ef7ed10d955b5f06
Matteo Fenu <matfen97@gmail.com>
2020-08-07 14:15:16 +0200

    edited the server to suit the build

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const logs = require('./api/logs')
-const path = require('path')
 require('dotenv').config()
 
 const middlewares = require('./middlewares')
@@ -37,14 +37,14 @@ app.get('/', (req, res) => {
 
 app.use('/api/logs', logs)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 // If a request makes it all the way   to here, it means the rout hasn't b een found
 app.use(middlewares.notFound)
 app.use(middlewares.errorHandler)
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('./client/build'))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, './client/build/index.html')))
-}
 
 
 const port = process.env.PORT || 5000
