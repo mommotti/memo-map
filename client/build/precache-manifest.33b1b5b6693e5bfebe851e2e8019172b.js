595bfa8bd60091e8870d9ee11560745d0f3d7c6c
Matteo Fenu <matfen97@gmail.com>
2020-08-07 13:43:11 +0200

    updated directory layout

---
+++ client/build/precache-manifest.33b1b5b6693e5bfebe851e2e8019172b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a965e5d9cf63944154ceb2b0e6294af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b809e989dde7a7f3007",
+    "url": "/static/css/main.517c81d1.chunk.css"
+  },
+  {
+    "revision": "81bad9843f1f612f4414",
+    "url": "/static/js/2.d482d34d.chunk.js"
+  },
+  {
+    "revision": "1ada509f2aa7b291836ed970b5de58bf",
+    "url": "/static/js/2.d482d34d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b809e989dde7a7f3007",
+    "url": "/static/js/main.b8f09be8.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
