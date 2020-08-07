a0121d7efef7f67ec6d2d005d659dfe4393413bb
Matteo Fenu <matfen97@gmail.com>
2020-08-07 10:17:24 +0200

    created build

--- server/src/index.js
+++ server/src/index.js
@@ -40,6 +40,12 @@ app.use('/api/logs', logs)
 app.use(middlewares.notFound)
 app.use(middlewares.errorHandler)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../../client/build'))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../../', 'client', 'build', 'index.html')))
+}
+
+
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`❗Server started at http://localhost:${port} ❗`))
