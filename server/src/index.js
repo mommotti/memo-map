c33214fa5f2a7b5283768c3c1f09f1ee46f723e7
Matteo Fenu <matfen97@gmail.com>
2020-08-07 11:33:26 +0200

    fixed the path

--- server/src/index.js
+++ server/src/index.js
@@ -42,7 +42,7 @@ app.use(middlewares.errorHandler)
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('../../client/build'))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../../', 'client', 'build', 'index.html')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../../client/build/index.html')))
 }
 
 
